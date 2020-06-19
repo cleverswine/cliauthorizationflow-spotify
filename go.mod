94c6e2c5a70b0f2b7333ee8210b98eb3394b4137
Noone, Kevin <kevin.noone@surescripts.com>
2020-06-18 17:19:52 -0700

    use spotify lib

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cleverswine/spotifycli
 
 go 1.14
 
-require github.com/cleverswine/cliauthorizationflow v0.1.4
+require (
+	github.com/cleverswine/cliauthorizationflow v0.1.6
+	github.com/zmb3/spotify v0.0.0-20200525010707-bc712583571e
+)
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"os"
 
 	auth "github.com/cleverswine/cliauthorizationflow"
+	"github.com/zmb3/spotify"
 )
 
 const (
@@ -25,73 +26,20 @@ func main() {
 		AuthorizationURL: SpotifyAuthURL,
 		TokenURL:         SpotifyTokenURL,
 		Scopes:           []string{"user-top-read"},
-		//CallbackPort:     8089,
 	}
 
-	client, err := auth.NewClient(ctx, config, nil)
+	client, err := auth.NewClient(ctx, config, auth.NewDefaultTokenStorage("spotify-cli"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Persist()
 
-	// get my top tracks
-	resp, err := client.Get(SpotifyAPIBase + "/me/top/tracks")
+	spotifyClient := spotify.NewClient(client.Client)
+	tracks, err := spotifyClient.CurrentUsersTopTracks()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// write them out to a json file
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = ioutil.WriteFile("top-tracks.json", body, os.ModePerm)
-	if err != nil {
-		log.Fatal(err)
+	for _, track := range tracks.Tracks {
+		fmt.Println(track.String())
 	}
 }
-
-// func getConfigFile() string {
-// 	osUser, err := user.Current()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	dir := path.Join(osUser.HomeDir, ".config/spot")
-// 	_, err = os.Stat(dir)
-// 	if os.IsNotExist(err) {
-// 		log.Println("creating directory " + dir)
-// 		errDir := os.MkdirAll(dir, 0600)
-// 		if errDir != nil {
-// 			log.Fatal(errDir)
-// 		}
-// 	}
-// 	return path.Join(dir, "t")
-// }
-
-// func tokenFromCache() (*oauth2.Token, error) {
-// 	fn := getConfigFile()
-// 	if _, err := os.Stat(fn); err != nil {
-// 		return nil, nil
-// 	}
-// 	f, err := os.Open(fn)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer f.Close()
-// 	t := &oauth2.Token{}
-// 	err = json.NewDecoder(f).Decode(t)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	return t, nil
-// }
-
-// func saveTokenToCache(token *oauth2.Token) error {
-// 	f, err := os.Create(getConfigFile())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer f.Close()
-// 	log.Println("saving token to " + f.Name())
-// 	return json.NewEncoder(f).Encode(token)
-// }
